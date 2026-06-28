61dd8985252df719e9b2200202326fcbd4b29436
Jan Vorloeper <98854306+vorlojan6@users.noreply.github.com>
2026-06-28 13:14:05 +0200

    rename `salsa8.py` -> `salsa.py` + fix salsa instantiation

--- src/civerly/cipher_implementations/salsa.py
+++ src/civerly/cipher_implementations/salsa.py
@@ -1,14 +1,14 @@
 from civerly.addrx import AddRX
 from civerly.component import RotateLayer_CVL, ModAdd_CVL, XOR_CVL
 
-class Salsa8QRF_CVL:
-    #Salsa8 quarter round function
+class SalsaQRF_CVL:
+    #Salsa quarter round function
     def __init__(self, name=None):
         r"""
         EXAMPLES::
             sage: from civerly.util import int_to_vec, vec_to_int
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8QRF_CVL
-            sage: qrf = Salsa8QRF_CVL()
+            sage: from civerly.cipher_implementations.salsa import SalsaQRF_CVL
+            sage: qrf = SalsaQRF_CVL()
             sage: hex(vec_to_int(qrf(int_to_vec(
             ....:   0x11111111_22222222_33333333_44444444, 
             ....: 128))))
@@ -16,9 +16,9 @@ class Salsa8QRF_CVL:
 
         TESTS::
             sage: # optional - cryptominisat
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8QRF_CVL
+            sage: from civerly.cipher_implementations.salsa import SalsaQRF_CVL
             sage: from civerly.model_options import *
-            sage: cipher = Salsa8QRF_CVL()
+            sage: cipher = SalsaQRF_CVL()
             sage: import tempfile
             sage: with tempfile.TemporaryDirectory() as tmpdir:
             ....:   model_options = MODEL_OPTIONS(
@@ -37,9 +37,9 @@ class Salsa8QRF_CVL:
             Output file in...
 
             sage: # optional - cadical
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8QRF_CVL
+            sage: from civerly.cipher_implementations.salsa import SalsaQRF_CVL
             sage: from civerly.model_options import *
-            sage: cipher = Salsa8QRF_CVL()
+            sage: cipher = SalsaQRF_CVL()
             sage: import tempfile
             sage: with tempfile.TemporaryDirectory() as tmpdir:
             ....:   model_options = MODEL_OPTIONS(
@@ -58,9 +58,9 @@ class Salsa8QRF_CVL:
             Output file in...
 
             sage: # optional - cryptominisat
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8QRF_CVL
+            sage: from civerly.cipher_implementations.salsa import SalsaQRF_CVL
             sage: from civerly.model_options import *
-            sage: cipher = Salsa8QRF_CVL()
+            sage: cipher = SalsaQRF_CVL()
             sage: import tempfile
             sage: with tempfile.TemporaryDirectory() as tmpdir:
             ....:   model_options = MODEL_OPTIONS(
@@ -75,7 +75,7 @@ class Salsa8QRF_CVL:
 
         """
         if name is None:
-            name = "Salsa8-QRF"
+            name = "Salsa-QRF"
         salsa_qr = AddRX(32, 4, 4, name=name)
         #modular addition operation
         add = ModAdd_CVL(32, name="add")
@@ -109,34 +109,34 @@ class Salsa8QRF_CVL:
         self.salsa_qr = salsa_qr
 
     def __new__(cls, *args, **kwargs):
-        instance = super(Salsa8QRF_CVL, cls).__new__(cls)
+        instance = super(SalsaQRF_CVL, cls).__new__(cls)
         instance.__init__(*args, **kwargs)
         return instance.salsa_qr
     
-class Salsa8_CVL:
-    # Salsa8 stream generator
+class Salsa_CVL:
+    # Salsa stream generator
     def __init__(self, R=8, name=None):
         r"""
         EXAMPLES::
             sage: input1 = (0x61707865_04030201_08070605_0c0b0a09_100f0e0d_3320646e_01040103_06020905_00000007_00000000_79622d32_14131211_18171615_1c1b1a19_201f1e1d_6b206574)
             sage: from civerly.util import int_to_vec, vec_to_int
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8_CVL
-            sage: salsa_cipher = Salsa8_CVL(2)
+            sage: from civerly.cipher_implementations.salsa import Salsa_CVL
+            sage: salsa_cipher = Salsa_CVL(2)
             sage: hex(vec_to_int(salsa_cipher(int_to_vec(input1, 512))))
             '0xba2409b11b7cce6a29115dcf5037e02737b75378348d94c83ea582b3c3a9a148825bfcb9226ae9eb63dd77487129a2154effd1ec5f25dc72a6c3d164152a26d8'
 
             sage: input2 = (0xa3b1c2d3_00112233_52ae30e8_89abcdef_13579bdf_2468ace0_0badc0de_cafef00d_11111111_22222222_33333333_44444444_55555555_66666666_77777777_88888888)
             sage: from civerly.util import int_to_vec, vec_to_int
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8_CVL
-            sage: salsa_cipher = Salsa8_CVL(2)
+            sage: from civerly.cipher_implementations.salsa import Salsa_CVL
+            sage: salsa_cipher = Salsa_CVL(2)
             sage: hex(vec_to_int(salsa_cipher(int_to_vec(input2, 512))))
             '0x41b1b31d1296df111676ee175668aea816ef51124f730621d8d14d921cbae414f95dea20d4c516f4bfe236aaf0c865e5b016204d5e17a39dbf703e2d89b4363a'
 
         TESTS::
             sage: # optional - cryptominisat
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8_CVL
+            sage: from civerly.cipher_implementations.salsa import Salsa_CVL
             sage: from civerly.model_options import *
-            sage: salsa = Salsa8_CVL(2)
+            sage: salsa_cipher = Salsa_CVL(2)
             sage: import tempfile
             sage: with tempfile.TemporaryDirectory() as tmpdir:
             ....:   model_options = MODEL_OPTIONS(
@@ -145,14 +145,14 @@ class Salsa8_CVL:
             ....:     granularity=GRANULARITY.BITWISE,
             ....:     sat_solver=SOLVER.CRYPTOMINISAT,
             ....:     path=Path(tmpdir))
-            ....:   salsa.salsa_cipher.analyse(model_options=model_options)
+            ....:   salsa_cipher.analyse(model_options=model_options)
             24544 variables and 59105 clauses were written to ...
             0
 
             sage: # optional - cryptominisat
-            sage: from civerly.cipher_implementations.salsa8 import Salsa8_CVL
+            sage: from civerly.cipher_implementations.salsa import Salsa_CVL
             sage: from civerly.model_options import *
-            sage: salsa = Salsa8_CVL(2)
+            sage: salsa_cipher = Salsa_CVL(2)
             sage: import tempfile
             sage: with tempfile.TemporaryDirectory() as tmpdir:
             ....:   model_options = MODEL_OPTIONS(
@@ -161,15 +161,15 @@ class Salsa8_CVL:
             ....:     granularity=GRANULARITY.BITWISE,
             ....:     sat_solver=SOLVER.CRYPTOMINISAT,
             ....:     path=Path(tmpdir))
-            ....:   salsa.salsa_cipher.analyse(model_options=model_options)
+            ....:   salsa_cipher.analyse(model_options=model_options)
             24576 variables and 67361 clauses were written to ...
             1
 
         """
         if name is None:
-            name = "Salsa8"
+            name = "Salsa"
         
-        salsa_qr = Salsa8QRF_CVL()
+        salsa_qr = SalsaQRF_CVL()
 
         salsa_cipher = AddRX(32, 16, 16, name=name)
         state = salsa_cipher.IN
@@ -217,9 +217,10 @@ class Salsa8_CVL:
             else:
                 state = apply_round(f"row_round_{r+1}", row_tuples, state)
         
-        salsa_cipher.add_output([(state, (i,i)) for i in range(16)])
+        salsa_cipher.add_output([(state, (i, i)) for i in range(16)])
         self.salsa_cipher = salsa_cipher
    
-    def __call__(self, state):
-        return self.salsa_cipher(state)
-
+    def __new__(cls, *args, **kwargs):
+        instance = super(Salsa_CVL, cls).__new__(cls)
+        instance.__init__(*args, **kwargs)
+        return instance.salsa_cipher
