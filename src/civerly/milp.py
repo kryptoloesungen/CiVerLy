1785a98ad6798c725190e98ae67775ad79e86ee8
Jan Vorloeper <98854306+vorlojan6@users.noreply.github.com>
2026-07-15 10:04:07 +0200

    add detailed documentation

--- src/civerly/milp.py
+++ src/civerly/milp.py
@@ -23,10 +23,22 @@ class MILP_CVL(MixedIntegerLinearProgram):
         [([1, 0], [4.0, 6.0]), ([2, 0], [-2.0, -1.0]), ([2, 1, 0], [3.0, 1.0, -1.0])]
         sage: [milp.backend.objective_coefficient(i) for i in range(milp.number_of_constraints())]
         [0.0, 3.0, -2.0]
-
-
     """
     def __init__(self, *args, **kwargs):
+        """
+        Initialize :class:``MILP_CVL``. The process is almost identical to the initialization
+        of SageMath's :class:``MixedIntegerLinearProgram`` except for the following points:
+
+        - the solver argument is always set to "GLPK", as CiVerLy doesn't use it anyway.
+          Instead, the external solvers specified by the model options are used (see ``solvers.py``).
+
+        - New attributes, to make them easier to access:
+            - ``vars`` -- dict[str -> MIPVariable]; Stores all MIPVariables created by :meth:``self.new_variable``.
+
+            - ``MILP_IN``, ``MILP_OUT`` -- MIPVariable; The input and output variables.
+
+            - ``X`` -- MIPVariable; The standard variable to be used for modeling.
+        """
         # always default to glpk solver for sage wrapper to minimize dependencies
         kwargs.pop("solver", None)
         super().__init__(*args, solver="GLPK", **kwargs)
@@ -41,6 +53,9 @@ class MILP_CVL(MixedIntegerLinearProgram):
         """
         Compares two MILP_CVL instances with each other by considering
         the corresponding dictionaries (see :meth:``to_dict``).
+        Comparing two :class:``MixedIntegerLinearProgram`` objects considers the 
+        objects themselves, not whether the MILPs they represent are actually equal,
+        which is done here.
 
         TESTS:
         
@@ -85,22 +100,22 @@ class MILP_CVL(MixedIntegerLinearProgram):
             ....:   milp2 = MILP_CVL.load(f.name)
             ....:   milp == milp2
             True
-
-
         """
         if not isinstance(other, MILP_CVL):
             return False
-
         return self.to_dict() == other.to_dict()
 
     @property
     def vars(self):
+        """
+        A dictionary containing all the variables added via :meth:``new_variable``.
+        """
         return self.__vars
 
     def new_variable(self, *args, **kwargs):
         """
         Override :meth:``MixedIntegerLinearProgram.new_variable`` to 
-        also store this variable as an attribute of ``self``.
+        also store this variable inside ``self.vars``.
         """
         name = kwargs.get("name")
         var = super().new_variable(*args, **kwargs)
@@ -108,6 +123,12 @@ class MILP_CVL(MixedIntegerLinearProgram):
         return var
 
     def dump(self, filename):
+        """
+        Serialize ``self`` into a dictionary using :meth:`to_dict`,
+        and write to ``filename`` afterwards. Takes the parameter:
+
+        - filename -- str; The json filename to dump ``self`` into.
+        """
         with open(filename, "w") as f:
             json.dump(self.to_dict(), fp=f)
         return
@@ -115,14 +136,86 @@ class MILP_CVL(MixedIntegerLinearProgram):
     def to_dict(self):
         """
         Make ``MILP_CVL`` json-serializable by creating a dictionary with 
-        all the relevant information.
+        the following information:
+
+        - maximization -- bool; Determines if the objective is to maximize or not.
+
+        - variables -- list of the form ``[(name, index, backend_index)]``, where:
+            - ``name`` -- string; The name of the MIPVariable object.
+            - ``index`` -- int; The index of this variable inside the MIPVariable object
+              (recall, MIPVariables are dictionaries)
+            - ``backend_index`` -- int; The index of the corresponding backend variable. 
+              In the backend, the variables are of the form ``x_1234``, ``backend_index``
+              stores the integer 1234.
+
+        - objective -- list of floats, indexed by the variables list; contains the coefficients 
+          for the objective function. If we would have ``x[0] - 2*x[1] + x[3]``, the 'objective'
+          list would be ``[1.0, -2.0, 0.0, 1.0]``.
+
+        - constraints -- list of the form ``[[appearing_vars, coeffs, rhs]]``, where:
+            - ``appearing_vars`` -- list of ints; the indices (under the variables list)
+              of the variables appearing in this constraint.
+            - ``coeffs`` -- list of floats; the corresponding coefficients of this constraint.
+            - ``rhs`` -- list of two ints; represent the lower (index 0) and
+              upper (index 1) bounds in this constraint.
+            An example would look as follows:
+            ``[..., [(611, 613, 614, 616, 617, 619), (-1.0, -1.0, 1.0, -1.0, -1.0, -1.0), [None, 0.0]], ...]``
+            translates to the constraint :math:``-variables[611] - variables[613] + variables[614]
+            - variables[616] - variables[617] - variables[619] \leq 0``
+            
+        EXAMPLE::
+
+            sage: from civerly.cipher_implementations.present \
+            ....:   import PRESENT_CVL
+            sage: from civerly.model_options import *
+            sage: import tempfile
+            sage: present_cipher = PRESENT_CVL(R=4)
+            sage: # optional - espresso
+            sage: with tempfile.TemporaryDirectory() as tmpdir:
+            ....:   model_options = MODEL_OPTIONS(
+            ....:     cryptanalysis=CRYPTANALYSIS.DIFFERENTIAL,
+            ....:     optimization=OPTIMIZATION.MILP,
+            ....:     granularity=GRANULARITY.BITWISE,
+            ....:     sbox_modeling=SBOX_MODELING.LOGICAL_COND_ESPRESSO,
+            ....:     logic_minimizer=SOLVER.ESPRESSO,
+            ....:     path=Path(tmpdir))
+            ....:   present_cipher.model(model_options)
+            5312 variables and 8641 constraints were written to ...
+            sage: present_cipher.milp.to_dict()['variables']
+            [('IN', 0, 5184),
+             ('IN', 1, 5185),
+             ('IN', 2, 5186),
+             ('IN', 3, 5187),
+             ('IN', 4, 5188),
+             ('IN', 5, 5189),
+             ('IN', 6, 5190),
+             ('IN', 7, 5191),
+             ('IN', 8, 5192),
+             ...
+             ('X6', 120, 5177),
+             ('X6', 123, 5178),
+             ('X6', 122, 5179),
+             ('X6', 125, 5180),
+             ('X6', 124, 5181),
+             ('X6', 127, 5182),
+             ('X6', 126, 5183)]
+            sage: present_cipher.milp.to_dict()['objective']
+            [0.0, 0.0, 0.0, 0.0, 0.0,...
+            sage: present_cipher.milp.to_dict()['constraints']
+            [[(0, 1), (-1.0, 1.0), [0.0, 0.0]],
+             [(2, 3), (-1.0, 1.0), [0.0, 0.0]],
+             [(4, 5), (-1.0, 1.0), [0.0, 0.0]],
+             [(6, 7), (-1.0, 1.0), [0.0, 0.0]],
+             [(8, 9), (-1.0, 1.0), [0.0, 0.0]],
+             ...
+
         """
         return {
             "maximization": self.backend.is_maximization(),
             "variables": [ # has the form [('x', 0, 0), ('x', 1, 1), ...]
-                (index, int(k), int(str(v)[2:]))
-                for index, var in list(self.vars.items())
-                for k, v in var.items()
+                (name, int(index), int(str(backend_index)[2:]))
+                for name, var in list(self.vars.items())
+                for index, backend_index in var.items()
             ],
             "objective": [
                 float(self.backend.objective_coefficient(i))
@@ -153,7 +246,40 @@ class MILP_CVL(MixedIntegerLinearProgram):
     @classmethod
     def from_dict(cls, data):
         """
-        Reconstruct a MILP_CVL from the output of :meth:``to_dict``.
+        Reconstruct a ``MILP_CVL`` from the output of :meth:``to_dict``,
+        by building the variables, objective function, and constraints
+        from the given dictionary ``data``.
+
+        INPUT:
+        - ``data`` -- dict; output of :meth:``to_dict``
+
+        OUTPUT: A ``MILP_CVL`` object corresponding to ``data``
+
+        EXAMPLE:
+
+            sage: from civerly.cipher_implementations.craft \
+            ....:   import CRAFT_CVL
+            sage: from civerly.model_options import *
+            sage: import tempfile
+            sage: cipher = CRAFT_CVL(R=4)
+            sage: # optional - espresso
+            sage: with tempfile.TemporaryDirectory() as tmpdir:
+            ....:   model_options = MODEL_OPTIONS(
+            ....:     cryptanalysis=CRYPTANALYSIS.DIFFERENTIAL,
+            ....:     optimization=OPTIMIZATION.MILP,
+            ....:     granularity=GRANULARITY.BITWISE,
+            ....:     linear_layer_modeling=LINEAR_LAYER_MODELING.MORE_DUMMIES,
+            ....:     sbox_modeling=SBOX_MODELING.LOGICAL_COND_ESPRESSO,
+            ....:     logic_minimizer=SOLVER.ESPRESSO,
+            ....:     path=Path(tmpdir))
+            ....:   cipher.model(model_options)
+            10176 variables and 12929 constraints were written to...
+            sage: data = cipher.milp.to_dict()
+            sage: from civerly.milp import MILP_CVL
+            sage: milp = MILP_CVL.from_dict(data)
+            sage: milp == cipher.milp
+            True
+
         """
         milp = cls(maximization=data["maximization"])
 
