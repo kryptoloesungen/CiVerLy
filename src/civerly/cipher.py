d637856af383ea80b043d6b0e95a034101e2324f
Jan Vorloeper <98854306+vorlojan6@users.noreply.github.com>
2026-08-14 13:11:35 +0200

    fix report inconsistency

--- src/civerly/cipher.py
+++ src/civerly/cipher.py
@@ -1873,7 +1873,7 @@ class Cipher:
             the weight. To re-read a SAT result, call :meth:`analyse` again.
         """
         # if the results are still stored, look there
-        if 0 < index < len(self.results):
+        if self.results and -len(self.results) <= index < len(self.results):
             return (
                 self.results[index]["assignment"],
                 self.results[index]["objective_value"],
